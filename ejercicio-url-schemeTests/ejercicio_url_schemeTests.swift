032024a1423493c9ec620d30770bc82d3887121a
Edgar A. Baeza <ebaeza1992@gmail.com>
2016-08-25 17:40:46 -0500

    Initial Commit

---
+++ ejercicio-url-schemeTests/ejercicio_url_schemeTests.swift
@@ -0,0 +1,36 @@
+//
+//  ejercicio_url_schemeTests.swift
+//  ejercicio-url-schemeTests
+//
+//  Created by Edgar Aparicio Baeza on 25/08/16.
+//  Copyright © 2016 Edgar Aparicio Baeza. All rights reserved.
+//
+
+import XCTest
+@testable import ejercicio_url_scheme
+
+class ejercicio_url_schemeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
