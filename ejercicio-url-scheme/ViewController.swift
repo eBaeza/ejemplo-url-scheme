90522605a83c2955c1f6dece01b0ccd1c9b82e50
Edgar A. Baeza <ebaeza1992@gmail.com>
2016-08-26 12:17:57 -0500

    Se mandan parametros por url scheme

--- ejercicio-url-scheme/ViewController.swift
+++ ejercicio-url-scheme/ViewController.swift
@@ -12,10 +12,13 @@ class ViewController: UIViewController, UIWebViewDelegate {
 
     @IBOutlet weak var webView: UIWebView!
     
+    @IBOutlet weak var userName: UITextField!
+    
     override func viewWillAppear(animated: Bool) {
         if let url = NSBundle.mainBundle().URLForResource("index", withExtension: "html", subdirectory: "web") {
             let fragUrl = NSURL(string: "#FRAG_URL", relativeToURL: url)!
             let request = NSURLRequest(URL: fragUrl)
+
             webView.delegate =  self
             webView.loadRequest(request)
         }
@@ -30,7 +33,19 @@ class ViewController: UIViewController, UIWebViewDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        if let scheme = request.URL?.scheme {
+            NSLog("kjdgjkdgjkgd \(request.URL)")
+            
+            if scheme ==  "knotionscheme" {
+                let urlComponents = NSURLComponents(URL: request.URL!, resolvingAgainstBaseURL: false)
+                let params = (urlComponents?.queryItems)! as [NSURLQueryItem]
+                userName.text = params[0].value! + " " + params[1].value!
+            }
+        }
+        
+        return true
+    }
 }
 
