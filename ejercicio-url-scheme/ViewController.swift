60e51a06d6ca16c183c21b909f51981794ea2796
Edgar A. Baeza <ebaeza1992@gmail.com>
2016-08-25 19:16:03 -0500

    Inicial

--- ejercicio-url-scheme/ViewController.swift
+++ ejercicio-url-scheme/ViewController.swift
@@ -8,8 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate {
 
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewWillAppear(animated: Bool) {
+        if let url = NSBundle.mainBundle().URLForResource("index", withExtension: "html", subdirectory: "web") {
+            let fragUrl = NSURL(string: "#FRAG_URL", relativeToURL: url)!
+            let request = NSURLRequest(URL: fragUrl)
+            webView.delegate =  self
+            webView.loadRequest(request)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
